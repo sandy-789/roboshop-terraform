56bd01e99285000b81a178e4df18f4325ff8d6e0
sandy-789 <sandeepchary125@gmail.com>
2024-02-17 15:53:20 +0530

    robosho-terraform

---
+++ 02.sG/variable.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+  default = "roboshop"
+}
+
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "sg_tags" {
+  default = {}
+}
